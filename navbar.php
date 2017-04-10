2112750fd50d4cf24153cecc04bf0dc1468eae08
senorhan <hansbrrix@gmail.com>
2017-04-10 15:19:08 -0700

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<?php
+
+<header class="container">
+    <div class="row">
+      <h1 class="col-sm-4"><a href="/index.html"/>Contour</h1>
+      <nav class="col-sm-8 text-right">
+        <p>Featured Stories</p>
+        <p><a class="menu-button" href="/about.html" role="button">About</p>
+        <p>Contact Us</p>
+      </nav>
+    </div>
+  </header>
